484c07d9ea143e23dcc5c887be59589caa03aa0f
Nicolás Margni <nicolasmargniup@gmail.com>
2026-01-23 03:52:04 -0300

    Deploy updated build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/nicolas-margni-portafolio/',
+    base: '/personal-portafolio/',
     plugins: [react()],
     build: {
         rollupOptions: {
